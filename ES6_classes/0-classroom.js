b3b06a1d4faa58b2b667797fe159f8f38feb77e9
farid-hashimzada <hashimzadafarid@gmail.com>
2024-05-19 15:28:10 +0400

    0-classroom.js

--- ES6_classes/0-classroom.js
+++ ES6_classes/0-classroom.js
@@ -1,5 +1,5 @@
 export default class ClassRoom {
-	constructor(maxstudentsize) {
-		this._maxStudentSize = maxStudentSize;
-	}
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
+  }
 }
